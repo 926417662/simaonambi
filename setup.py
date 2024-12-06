4f0939a80caf81c5417d4aa4bb73f56e915a02b2
Nambi <simaonambi@gmail.com>
2024-12-06 10:21:42 +0000

    second commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='simaonambi',
+      version='1.0',
+      description='simaonambi Distribution Module',
+      author='simao',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['simaonambi'],
+     )
